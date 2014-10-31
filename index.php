98d7c4ec7f6540250fa8883114b67839c9c9318b
Kirens <eriknygren97@gmail.com>
2014-10-31 18:11:42 +0100

    Began mobile site css
    Card layout in the works

---
+++ index.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Hem | Blackebergs elevkår</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
+    <link rel="stylesheet" type="text/css" href="main.css">
+</head>
+<body>
+    <div id="menu">
+    </div>
+    <div id="slide">
+    </div>
+    <div id="content">
+        <div class="card">
+            <span class="info">
+                <span class="img-container"><img src=""/></span>
+                <h2>Lorem ipsum</h2>
+                <p>
+                    <span rel="author">Alexander Uggla Uggla</span>
+                    <span>2014-09-03 15:27</span>
+                </p>
+            </span>
+            <summary>Lorem ipsum dolor sit amet, in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum...</summary>
+        </div>
+        <div class="card">
+            <span class="info">
+                <span class="img-container"><img src=""/></span>
+                <h2>Rorem ipsum</h2>
+                <p>
+                    <span rel="author">Erik Nygren</span>
+                    <span>2014-09-03 15:28</span>
+                </p>
+            </span>
+            <summary>Rorem ipsum dolor sit amet, in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum...</summary>
+        </div>
+    </div>
+    <footer>
+    
+    </footer>
+</body>
+</html>

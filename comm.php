08e6faa6470c766bc499a95d8a0562093a0e28cb
John Daniel <daniel@bosser.com>
2014-11-06 15:09:59 +0100

    started communities page

---
+++ comm.php
@@ -0,0 +1,52 @@
+<DOCTYPE html>
+<html>
+    <head>
+        <link rel="stylesheet" type="text/css" href="index.css">
+        <meta charset="utf-8">
+        
+        <link rel="stylesheet" type="text/css" href="comm.css">
+    </head>
+    
+    <body>
+        <div id="wrapper">
+            <?php include "navigation.php"; ?>
+            
+            <header>
+                <div>
+                    <h1>
+                        Föreningar
+                    </h1>
+                    
+                    <p>
+                        En intressant text om föreningarna på skolan ska stå här
+                    </p>
+                </div>
+            </header>
+            
+            <main>
+                <article id="programmeringsföreningen" class="card">
+                    <img />
+                    <h1>akaProxy: Programmeringsföreningen</h1>
+                    <p>En intressant text om föreningen ska stå här.</p>
+                </article>
+                <article id="debattföreningen" class="card">
+                    <img />
+                    <h1>Debattklubben</h1>
+                    <p>En intressant text om föreningen ska stå här.</p>
+                </article>
+                <article id="debattföreningen" class="card">
+                    <img />
+                    <h1>Debattklubben</h1>
+                    <p>En intressant text om föreningen ska stå här.</p>
+                </article>
+                <article id="debattföreningen" class="card">
+                    <img />
+                    <h1>Debattklubben</h1>
+                    <p>En intressant text om föreningen ska stå här.</p>
+                </article>
+            </main>
+            
+            <?php include "footer.php"; ?>
+        </div>
+    </body>
+</html>

8b4750f189654c134007b14b0296af43ce65a33f
John Daniel <daniel@bosser.com>
2014-11-07 16:13:55 +0100

    något

--- comm.php
+++ comm.php
@@ -1,33 +1,36 @@
 <DOCTYPE html>
-<html>
+    <html>
+
     <head>
         <link rel="stylesheet" type="text/css" href="index.css">
         <meta charset="utf-8">
-        
+
         <link rel="stylesheet" type="text/css" href="comm.css">
     </head>
-    
+
     <body>
         <div id="wrapper">
             <?php include "navigation.php"; ?>
-            
+
             <header>
                 <div>
                     <h1>
                         Föreningar
                     </h1>
-                    
+
                     <p>
                         En intressant text om föreningarna på skolan ska stå här
                     </p>
                 </div>
             </header>
-            
+
             <main>
                 <article id="programmeringsföreningen" class="card">
                     <img />
-                    <h1>akaProxy: Programmeringsföreningen</h1>
-                    <p>En intressant text om föreningen ska stå här.</p>
+                    <div>
+                        <h1>akaProxy: Programmeringsföreningen</h1>
+                        <p>En intressant text om föreningen ska stå här.</p>
+                    </div>
                 </article>
                 <article id="debattföreningen" class="card">
                     <img />
@@ -45,8 +48,9 @@
                     <p>En intressant text om föreningen ska stå här.</p>
                 </article>
             </main>
-            
+
             <?php include "footer.php"; ?>
         </div>
     </body>
+
 </html>
